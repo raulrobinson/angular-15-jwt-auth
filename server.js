f30a1e5d772bb17037708f8b32e023e8af229334
Raul Bolivar <raul.bolivar.ext@telefonica.com>
2023-01-28 20:43:45 -0500

    deploy dist node

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/heroku-deploy'));
+app.use(express.static(__dirname + '/dist/angular-15-jwt-auth'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/heroku-deploy/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/angular-15-jwt-auth/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
