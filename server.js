fd5c1f0d752704fca4c1f32fab9b8e39a37097a0
Raul Bolivar <raul.bolivar.ext@telefonica.com>
2023-01-29 09:38:06 -0500

    cors headers sent

--- server.js
+++ server.js
@@ -15,8 +15,10 @@ app.use(express.json());
 
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', 'https://angular-login-jwt.herokuapp.com');
+  res.setHeader('Access-Control-Allow-Credentials', true)
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+  res.setHeader('x-access-token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjNkNjZhZjM1OGEyYzE1N2M0Y2I4MTQwIiwiZW1haWwiOiJyYXN5c2JveEBob3RtYWlsLmNvbSIsImlhdCI6MTY3NTAwMDk4MywiZXhwIjoxNjc1MDA4MTgzfQ.N4CUIYPb5rPgirVzG_-d_DDLOuUNmy05ciFWZMQYzo8');
   next();
 });
 
