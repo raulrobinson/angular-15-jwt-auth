3214277d4a94b7e16575d8857112cb3c4cf3333c
Raul Bolivar <raul.bolivar.ext@telefonica.com>
2023-01-28 20:52:31 -0500

    cors in node js

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const path = require('path');
 
+/* Activamos manejo de CORS */
+const cors = require('cors');
+
+/* Utilizamos los CORS */
+app.use(cors())
+
 const app = express();
 
 app.use(express.static(__dirname + '/dist/angular-15-jwt-auth'));
 
 app.get('/*', function(req,res) {
-
   res.sendFile(path.join(__dirname+'/dist/angular-15-jwt-auth/index.html'));
 });
 
