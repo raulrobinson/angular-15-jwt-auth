912c4c6d5e5ed01e54b1944b6437d6f0b1181e15
Raul Bolivar <raul.bolivar.ext@telefonica.com>
2023-01-28 19:58:59 -0500

    first commit

---
+++ src/app/_services/auth.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { environment } from "../../environments/environment"
+
+/* Localmente */
+const AUTH_API = 'http://localhost:9001/api/auth/';
+//const AUTH_API = 'https://api-login-jwt-mock.herokuapp.com/api/auth/';
+
+/* Variables de Entorno */
+const url = environment.Baseurl
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) {}
+
+  login(username: string, password: string): Observable<any> {
+    return this.http.post(
+      AUTH_API + 'signin',
+      {
+        username,
+        password,
+      },
+      httpOptions
+    );
+  }
+
+  register(username: string, email: string, password: string): Observable<any> {
+    return this.http.post(
+      AUTH_API + 'signup',
+      {
+        username,
+        email,
+        password,
+      },
+      httpOptions
+    );
+  }
+
+  logout(): Observable<any> {
+    return this.http.post(AUTH_API + 'signout', { }, httpOptions);
+  }
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  Baseurl: "https://api-login-jwt-mock.herokuapp.com/api/auth/"
+};
